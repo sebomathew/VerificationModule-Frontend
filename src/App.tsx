442c1f074c85d4c8bef1b7628cb6938c31eab409
Sebo800996 <sebo.mathew@macomsolutions.com>
2026-09-22 11:33:47 +0530

    Initial React frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+            <h1>Verification Module</h1>
+            <p>Welcome! This is our React frontend running on Vute.</p>
+
+            <button onClick={() => alert("Button clicked!")}>
+                Test Action
+            </button>
+        </div>
+    );
+}
+
+export default App;
